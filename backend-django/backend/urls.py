4b6e3ed9b09ee24ae30ed641737ce9379fb70477
Ade Musytahun Wahid <ademusytahunwahid@gmail.com>
2025-09-29 22:26:54 +0800

    feat: tambah url / hilang karena strawberry

--- backend-django/backend/urls.py
+++ backend-django/backend/urls.py
@@ -2,8 +2,13 @@ from django.contrib import admin
 from django.urls import path
 from strawberry.django.views import GraphQLView
 from .schema import schema
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("Welcome to Django backend. GraphQL is at /graphql/")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('graphql/', GraphQLView.as_view(schema=schema)),
+    path('', home),
 ]
